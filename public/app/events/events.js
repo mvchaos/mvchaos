dec0de9f883570af11bdc9572820d7e510a3b69d
Dan Novograd <dnovograd@gmail.com>
2016-02-12 17:55:30 -0800

    Integrated ZipCode API

--- public/app/events/events.js
+++ public/app/events/events.js
@@ -1,16 +1,22 @@
 angular.module('booletin.events',[])
 
-.controller('EventController', function ($scope, Events){
-  $scope.location = {};
+.controller('EventController', function ($scope, Events, $state){
+  $scope.queryZip = {};
   $scope.events = {};
   $scope.getEvents = function (){
-    Events.queryLocation($scope.events)
+    Events.queryLocation($scope.queryZip)
       .then(function (events){
-        $scope.events = events;
+        Events.targetZips = [];
+        Events.lastLookup = $scope.queryZip.zipcode;
+        for (var i = 0; i < events.data[0].zip_codes.length; i++) {
+          Events.targetZips.push(events.data[0].zip_codes[i].zip_code)
+        }
+        $state.go('events');
       })
       .catch(function(error){
         console.error(error);
       });
   };
-  $scope.getEvents();
+  console.log("Saved Lookup", Events.lastLookup);
+  console.log("Target Zips", Events.targetZips);
 });

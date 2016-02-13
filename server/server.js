900b28cd62a45d47b42f05818c439cac79430670
Dan Novograd <dnovograd@gmail.com>
2016-02-12 17:55:30 -0800

    Integrated ZipCode API

--- server/server.js
+++ server/server.js
@@ -4,17 +4,42 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 4569;
-
-app.post('/api/events', function(req, res){
-  console.log('simple form post');
-  res.send(req.query.data);
-});
-
-app.get('/api/events', function(req, res){
-  console.log('simple form get');
-  res.send('simple form');
-});
-
+var Promise = require('bluebird');
+var request = require('request');
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/../public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.listen(port);
+
+app.get('/api/events', function(req, res) {
+    res.send('simple form');
+});
+
+var apiString = 'http://www.zipcodeapi.com/rest/gEX2rYvCM8X5XH2fFjbbmSXVZooExeONTMQlKmXxuVDsCFHiZTtmMrhLPGqmVeaP/radius.json/'
+
+var apiCall = function(data) {
+    return new Promise(function(resolve, reject) {
+        request(apiString + data.zipcode + '/1/mile/',
+            function(error, response, body) {
+                if (error) {
+                    return console.log('Error:', error);
+                }
+                if (response.statusCode !== 200) {
+                    return console.log('Invalid Status Code Returned:', response.statusCode);
+                }
+                var answer = JSON.parse(body);
+                resolve(answer);
+            });
+    });
+};
+
+app.post('/api/location', function(req, res) {
+    var firstpromise = apiCall(req.body);
+    Promise.all([firstpromise]).then(function(results) {
+        res.send(results);
+    }).catch(function() {});
+});

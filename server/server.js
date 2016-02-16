2bd6f31b6b7054889add0fdec9b1dcd189ca449f
James Youn <jamesyoun710@gmail.com>
2016-02-16 12:28:23 -0800

    (front-end)Add dependencies as npm modules

--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use('/scripts', express.static(__dirname + '/../node_modules/'));
+
+
 app.listen(port);
 
 app.get('/api/events', function(req, res) {

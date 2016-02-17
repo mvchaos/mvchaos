54741d87905412170f47936004d763f20c244ae0
Dan Novograd <dnovograd@gmail.com>
2016-02-16 18:23:03 -0800

    Cleaned up events page

--- public/app/events/events.js
+++ public/app/events/events.js
@@ -3,17 +3,20 @@ angular.module('booletin.events',[])
 .controller('EventController', function ($scope, Events, $state, $firebaseArray){
   var dbConnection = new Firebase("https://booletin.firebaseio.com/events");
   $scope.events = $firebaseArray(dbConnection);
-
+  $scope.targetZipsString = Events.targetZipsString;
   $scope.queryZip = {};
   // $scope.events = {};
   $scope.getEvents = function (){
     Events.queryLocation($scope.queryZip)
       .then(function (response){
         Events.targetZips = [];
+        Events.targetZipsString = "";
         Events.lastLookup = $scope.queryZip.zipcode;
         for (var i = 0; i < response.data[0].zip_codes.length; i++) {
-          Events.targetZips.push(response.data[0].zip_codes[i].zip_code)
+          Events.targetZips.push(response.data[0].zip_codes[i].zip_code);
+          Events.targetZipsString += (response.data[0].zip_codes[i].zip_code + ", ");
         }
+        Events.targetZipsString = Events.targetZipsString.slice(0, Events.targetZipsString.length - 2);
         $state.go('events');
       })
       .catch(function(error){
@@ -22,4 +25,5 @@ angular.module('booletin.events',[])
   };
   console.log("Saved Lookup", Events.lastLookup);
   console.log("Target Zips", Events.targetZips);
+  console.log("Target Zips List", Events.targetZipsString);
 });

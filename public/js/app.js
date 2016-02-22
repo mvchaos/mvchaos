9f8f98a0f8b9394fc6c440d76e33dc69254359fe
Dan Novograd <dnovograd@gmail.com>
2016-02-22 12:27:28 -0800

    fixed spacing

--- public/js/app.js
+++ public/js/app.js
@@ -1,50 +1,51 @@
-angular.module('booletin',[
-  'ngMap',
-  'booletin.services',
-  'booletin.events',
-  'booletin.add',
-  'ui.router',
-  'firebase'])
-.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/search');
-  $stateProvider
-    .state('search',{
-      url: '/search',
-      templateUrl: "search.html",
-      controller: 'EventController'
-    })
-    .state('add', {
-      url: '/add',
-      templateUrl: 'add.html',
-      controller: 'addEvents'
-    })
-    .state('events', {
-      url: '/events/:search',
-      templateUrl: 'events.html',
-      controller: 'EventController'
-    })
-})
-.controller('mapController', function($scope, NgMap,$http) {
-  $scope.loc = {}
-
-  $http({
-    method: 'GET',
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+encodeURIComponent($scope.event.streetAddress)+'&key=AIzaSyC7IPmYQhDK-tr4w9i9DLflO3ahjeuAbxc'
-  }).then(function successCallback(response) {
-    $scope.loc.x = response.data.results[0].geometry.location.lat || 1;
-    $scope.loc.y = response.data.results[0].geometry.location.lng || 1;
-  }, function errorCallback(response) {
-    console.log('not valid address',response);
+angular.module('booletin', [
+    'ngMap',
+    'booletin.services',
+    'booletin.events',
+    'booletin.add',
+    'ui.router',
+    'firebase'
+  ])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/search');
+    $stateProvider
+      .state('search', {
+        url: '/search',
+        templateUrl: "search.html",
+        controller: 'EventController'
+      })
+      .state('add', {
+        url: '/add',
+        templateUrl: 'add.html',
+        controller: 'addEvents'
+      })
+      .state('events', {
+        url: '/events/:search',
+        templateUrl: 'events.html',
+        controller: 'EventController'
+      })
   })
-        NgMap.getMap().then(function(map) {
-            $scope.map = map;
-        });
+  .controller('mapController', function($scope, NgMap, $http) {
+    $scope.loc = {}
+
+    $http({
+      method: 'GET',
+      url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURIComponent($scope.event.streetAddress) + '&key=AIzaSyC7IPmYQhDK-tr4w9i9DLflO3ahjeuAbxc'
+    }).then(function successCallback(response) {
+      $scope.loc.x = response.data.results[0].geometry.location.lat || 1;
+      $scope.loc.y = response.data.results[0].geometry.location.lng || 1;
+    }, function errorCallback(response) {
+      console.log('not valid address', response);
     })
+    NgMap.getMap().then(function(map) {
+      $scope.map = map;
+    });
+  })
 
 .directive('customOnChange', function() {
   return {
     restrict: 'A',
-    link: function (scope, element, attrs) {
+    link: function(scope, element, attrs) {
       var onChangeHandler = scope.$eval(attrs.customOnChange);
       element.bind('change', onChangeHandler);
     }
@@ -58,18 +59,18 @@ angular.module('booletin',[
     link: function(scope, elm, attrs, ctrl) {
       ctrl.$asyncValidators.validZip = function(modelValue, viewValue) {
         var def = $q.defer();
-        var viewObject =  {
-           zipcode : viewValue
+        var viewObject = {
+          zipcode: viewValue
         }
 
         Events.queryLocation(viewObject)
-        .then(function(response){
-          if(response.data[0]['zip_codes']){
-            def.resolve();
-          } else {
-            def.reject();
-          }
-        });
+          .then(function(response) {
+            if (response.data[0]['zip_codes']) {
+              def.resolve();
+            } else {
+              def.reject();
+            }
+          });
         return def.promise;
       };
     }
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ var bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/../public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 app.use('/scripts', express.static(__dirname + '/../node_modules/'));
 
@@ -19,7 +19,7 @@ app.use('/scripts', express.static(__dirname + '/../node_modules/'));
 app.listen(port);
 
 app.get('/api/events', function(req, res) {
-    res.send('simple form');
+  res.send('simple form');
 });
 
 var apiString = 'http://www.zipcodeapi.com/rest/13VmpoOcKPi4T3uLb7JDa2Kji98W6Yr71nurLiDw8WA2Z5NbcwOnzzGRIOOOMgWO/radius.json/'
@@ -27,21 +27,21 @@ var apiString = 'http://www.zipcodeapi.com/rest/13VmpoOcKPi4T3uLb7JDa2Kji98W6Yr7
 var apiCall = function(data) {
   return new Promise(function(resolve, reject) {
     request(apiString + data.zipcode + '/1/mile/',
-            function(error, response, body) {
-              if (error) {
-                console.log("error: ", error);
-              }
-              var answer = JSON.parse(body);
-              console.log(answer);
-              resolve(answer);
-            });
+      function(error, response, body) {
+        if (error) {
+          return console.log("error: ", error);
+        }
+        var answer = JSON.parse(body);
+        console.log(answer);
+        resolve(answer);
+      });
   });
 };
 
 app.post('/api/location', function(req, res) {
   console.log('req body: ', req.body);
-    var firstpromise = apiCall(req.body);
-    Promise.all([firstpromise]).then(function(results) {
-        res.send(results);
-    }).catch(function() {});
+  var firstpromise = apiCall(req.body);
+  Promise.all([firstpromise]).then(function(results) {
+    res.send(results);
+  }).catch(function() {});
 });
